f340772f537d6ed80e7fd9e503e7da5cc2be78ab
imperiumdigital1999-max <imperium.digital1999@gmail.com>
2025-07-29 18:21:42 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('avaliador-cache').then(cache => {
-      return cache.addAll(['/', '/index.html']);
+      return cache.addAll(['index.html']);
     })
   );
 });
